88fa514818b4bdfdff2adcaddc0e548574ec7660
Jonathan Boellke <kaakans@gmail.com>
2017-09-02 23:07:49 +0200

    More layout changes

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,22 +5,26 @@ export default class Contact extends Component {
 		return (
 			<div id="contact">
 				<h1 className="secondary-title">Kontakt</h1>
-				<table>
-                    <tbody>
-                        <tr>
-                            <td width="40"><i className="fa fa-facebook-official" /></td>
-                            <td><a href="http://facebook.com/fightfitkristinehamn">FightFit på facebook</a></td>
-                        </tr>
-                        <tr>
-                            <td width="40"><i className="fa fa-phone" /></td>
-                            <td>076-046 69 94</td>
-                        </tr>
-                        <tr>
-                            <td width="40"><i className="fa fa-map-marker" /></td>
-                            <td>Tredje Industrigatan 2, Kristinehamn</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <p>
+                    <div>
+                        <span className="icon-wrapper">
+                            <i className="fa fa-facebook-official" />
+                        </span>
+                        <a href="http://facebook.com/fightfitkristinehamn">FightFit på facebook</a>
+                    </div>
+                    <div>
+                        <span className="icon-wrapper">
+                            <i className="fa fa-phone" />
+                        </span>
+                        076-046 69 94
+                    </div>
+                    <div>
+                        <span className="icon-wrapper">
+                            <i className="fa fa-map-marker" />
+                        </span>
+                        Tredje Industrigatan 2, Kristinehamn
+                    </div>
+                </p>
 			</div>
 		);
 	}
